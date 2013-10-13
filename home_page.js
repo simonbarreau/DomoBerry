b842e3c43566de13e89270b34ff20d5c7c10f472
simonbarreau <simonbarreau@hotmail.fr>
2013-10-13 21:01:37 +0200

    citation + bouton
    - Ajout des citation ;
    - amélioration des boutons avec ajout de bordures;
    - création d'un bouton blanc.

--- home_page.js
+++ home_page.js
@@ -7,5 +7,15 @@ function font_picture(){
 }
 
 function citation(){
-	var liste_citation = ["« Ma vie est mon seul enseignement.  » de Gandhi</br>"];
+	var citation = [
+	"« Ma vie est mon seul enseignement.  » </br><i>- Gandhi",
+	"« Il vaut mieux mettre son coeur dans la prière sans</br> trouver de paroles que trouver des mots sans y mettre son coeur.  »</br><i>- Gandhi",
+	"« La vie est un mystère qu'il faut vivre, et non un problème à résoudre.  »</br><i>- Gandhi",
+	"« L'hésitation est le propre de l'intelligence.  »</br><i>- Henry de Montherlant",
+	"« La joie n'est pas dans les choses, elle est en nous.  »</br><i>- Richard Wagner",
+	"« Les fautes sont grandes quand l'amour est petit.  »</br><i>- Proverbe italien"
+	];
+	var random_citation = Math.floor(Math.random()*6);
+	
+	document.getElementById("citation").firstChild.innerHTML = citation[random_citation]+"</i>";
 }
